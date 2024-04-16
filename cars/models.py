18c7f1a475b9529d0707d50f5d53de9ea1f9ed82
Natan Ramos Ferreira <natanferreira.dev@gmail.com>
2024-04-15 22:09:10 -0300

    Aula nova

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model): # o Nome da classe vai ser o nome da tabela
+    id = models.AutoField(primary_key=True) #Auto Preenchimento no Banco
+    
