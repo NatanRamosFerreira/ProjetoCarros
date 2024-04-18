03f6d127796e9fc92841b6aa91e5c3cc10a6195b
Natan Ramos Ferreira <natanferreira.dev@gmail.com>
2024-04-17 21:11:09 -0300

    Criando A tabela  Car no Banco de Dados

--- cars/models.py
+++ cars/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 class Car(models.Model): # o Nome da classe vai ser o nome da tabela
     id = models.AutoField(primary_key=True) #Auto Preenchimento no Banco
+    model = models.CharField(max_length=200) #Modelo do carro
+    brand = models.CharField(max_length=200) #Marca
+    factory_year = models.IntegerField(blank=True, null=True) #Ano Fabricação
+    model_year = models.IntegerField(blank=True, null=True) # Ano do Modelo
+    value = models.FloatField(blank=True, null=True) # Valor do carro
+    
+
     
